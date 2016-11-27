38b108a3690dada00add9857c7cccdd0c797bab7
Timofey Kondrashov <tkondrashov@gmail.com>
2016-11-27 09:16:11 +0000

    everything is moved

---
+++ pericog/lib/daemons/template.php
@@ -0,0 +1,25 @@
+<?php
+$config = parse_ini_file("/srv/etc/config/daemons.ini", true);
+
+$last_runtime = file_get_contents("/etc/ochre/runtimes/" . DAEMON);
+if (!$last_runtime)
+{
+	$last_runtime = time() - $config['timing']['period'];
+}
+
+$db = new mysqli("localhost", DAEMON, file_get_contents("/srv/auth/daemons/" . DAEMON . ".pw"));
+if ($db->connect_error)
+{
+	error_log("Database connection failed for " . DAEMON . " daemon: " . $db->connect_error);
+}
+
+while (1)
+{
+	if (time() - $last_runtime > $config['timing']['period'])
+	{
+		daemon($db, $last_runtime, $config);
+		file_put_contents("/etc/ochre/runtimes/" . DAEMON, $last_runtime);
+		$last_runtime += $config['timing']['period'];
+	}
+	sleep(1);
+}
