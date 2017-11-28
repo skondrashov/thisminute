b70893faad7837b780c5addae908b893dc4f206d
Timofey Kondrashov <tkondrashov@gmail.com>
2017-11-27 20:48:22 -0500

    streamline local pericog creation by using TM_PERICOG_ADDRESS as flag

--- README.md
+++ README.md
@@ -1,24 +1,34 @@
-don't forget to clone recursively! this project contains boost as a submodule, so expect cloning to take a long time (~40 min)
-`git clone --recursive https://github.com/tkondrashov/ThisMinute`
+### Installation
+1) Be on Ubuntu 16.04
 
-
-/util/bash_aliases.sh has convenient aliases for setting up the remote servers. add the following lines to ~/.bashrc to access them from your terminal (logout required):
-
+2) Paste the following into a terminal:
 ```
-export TM_BASE_PATH=<path to ThisMinute repo>
-export TM_KEY_PATH=<path to ThisMinute private key>
-. $TM_BASE_PATH/util/bash_aliases.sh
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -;\
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list;\
+sudo apt-get update;\
+sudo apt-get -y install git sublime-text mysql-client python-pip;\
+git clone --recursive https://github.com/tkondrashov/thisminute ~/thisminute
 ```
 
+3)
+Add the following lines to the end of your .bashrc file (`subl ~/.bashrc` in Terminal to open). This will set you up for a local pericog installation which is most likely what you want. However, you can edit them to point to other servers ie `TM_PERICOG_ADDRESS="pericog.thisminute.org"`.
+```
+TM_MYSQL_ROOT_PASSWORD="paradise"
+TM_SENTINEL_ADDRESS="thisminute.org"
+TM_ARCHIVIST_ADDRESS="archivist.thisminute.org"
+TM_PERICOG_ADDRESS="localhost"
+. ~/thisminute/util/bash_aliases.sh
+```
 
-### pericog
-pericog server requires
+4) To install pericog, run this script on the machine you want pericog on (usually your machine). You will have to enter a password for your mysql root user. This MUST be the TM_MYSQL_ROOT_PASSWORD from step 3 above.
 ```
-sudo chmod 777 /srv
-sudo apt-get install python-pip g++-7 libmysqlcppconn-dev
-pip3 install mysql-connector==2.1.4 numpy scipy unidecode
-pip3 install gensim tensorflow-gpu
+sudo mkdir /srv;\
+sudo chmod 777 /srv;\
+sudo apt-get -y install mysql-server;\
+sudo pip install mysql-connector==2.1.4 numpy scipy unidecode gensim tensorflow-gpu;\
+pericog_init
 ```
+
 
 pericog compile command:
 g++-7 -I/srv/lib/mysql-connector-cpp/include -I/srv/lib/boost -I/srv/lib/inih/cpp -I/usr/include/cppconn -I/srv/lib -Wall -Werror -pedantic -std=c++14 /srv/etc/pericog.cpp /srv/lib/inih/cpp/INIReader.cpp /srv/lib/inih/ini.c -o /srv/bin/pericog -L/usr/lib -lmysqlcppconn -lpthread -O3
--- util/bash_aliases.sh
+++ util/bash_aliases.sh
@@ -1,9 +1,11 @@
-TM_SENTINEL_ADDRESS="tkondrashov@thisminute.org"
-TM_ARCHIVIST_ADDRESS="tkondrashov@archivist.thisminute.org"
-TM_PERICOG_ADDRESS="chelsea@localhost"
+TM_BASE_PATH=~/thisminute
+TM_KEY_PATH=~/thisminute/auth/ssl/tm.pem
 
 tm_push() {
 	rsync -uavz -e "ssh -i ${TM_KEY_PATH}" --chmod=777 --del --delete-excluded --exclude '*.git*' "$@"
+}
+tm_local_push() {
+	rsync -uav --chmod=777 --del --delete-excluded --exclude '*.git*' "$@"
 }
 
 tm_generate_auth() {
@@ -30,50 +32,43 @@ archivist_push() {
 
 pericog() { ssh -i $TM_KEY_PATH $TM_PERICOG_ADDRESS; }
 pericog_push() {
-	tm_push                 $TM_BASE_PATH/config.ini                      $TM_PERICOG_ADDRESS:/srv/config.ini;
-	tm_push --exclude 'lib' $TM_BASE_PATH/pericog/                        $TM_PERICOG_ADDRESS:/srv/etc/;
-	tm_push                 $TM_BASE_PATH/auth/                           $TM_PERICOG_ADDRESS:/srv/auth/;
-	tm_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ $TM_PERICOG_ADDRESS:/srv/lib/;
+	if [ $TM_PERICOG_ADDRESS == "localhost" ]
+	then
+		tm_local_push                 $TM_BASE_PATH/config.ini                      /srv/config.ini;
+		tm_local_push --exclude 'lib' $TM_BASE_PATH/pericog/                        /srv/etc/;
+		tm_local_push                 $TM_BASE_PATH/auth/                           /srv/auth/;
+		tm_local_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ /srv/lib/;
+	else
+		tm_push                 $TM_BASE_PATH/config.ini                      $TM_PERICOG_ADDRESS:/srv/config.ini;
+		tm_push --exclude 'lib' $TM_BASE_PATH/pericog/                        $TM_PERICOG_ADDRESS:/srv/etc/;
+		tm_push                 $TM_BASE_PATH/auth/                           $TM_PERICOG_ADDRESS:/srv/auth/;
+		tm_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ $TM_PERICOG_ADDRESS:/srv/lib/;
+	fi
 }
 pericog_db_init() {
-	read -s -p "Enter password: " password
-
-	cat "$TM_BASE_PATH/util/pericog.sql" |
-	sed "s/\$PW_TWEET2VEC/"$( cat $TM_BASE_PATH/auth/mysql/tweet2vec.pw )"/g" |
-	sed "s/\$PW_PERICOG/"$(   cat $TM_BASE_PATH/auth/mysql/pericog.pw   )"/g" |
-	ssh -i $TM_KEY_PATH $TM_PERICOG_ADDRESS "sudo mysql -u root -p$password";
+	if [ $TM_PERICOG_ADDRESS == "localhost" ]
+	then
+		cat "$TM_BASE_PATH/util/pericog.sql" |
+		sed "s/\$PW_TWEET2VEC/"$( cat $TM_BASE_PATH/auth/mysql/tweet2vec.pw )"/g" |
+		sed "s/\$PW_PERICOG/"$(   cat $TM_BASE_PATH/auth/mysql/pericog.pw   )"/g" |
+		sudo mysql -u root -p$TM_MYSQL_ROOT_PASSWORD;
+	else
+		cat "$TM_BASE_PATH/util/pericog.sql" |
+		sed "s/\$PW_TWEET2VEC/"$( cat $TM_BASE_PATH/auth/mysql/tweet2vec.pw )"/g" |
+		sed "s/\$PW_PERICOG/"$(   cat $TM_BASE_PATH/auth/mysql/pericog.pw   )"/g" |
+		ssh -i $TM_KEY_PATH $TM_PERICOG_ADDRESS "sudo mysql -u root -p$TM_MYSQL_ROOT_PASSWORD";
+	fi
 }
 pericog_init() {
 	pericog_db_init;
 	pericog_push;
 }
-local_pericog_push() {
-	local_push() {
-		rsync -uav --chmod=777 --del --delete-excluded --exclude '*.git*' "$@"
-	}
-	local_push                 $TM_BASE_PATH/config.ini                      /srv/config.ini;
-	local_push --exclude 'lib' $TM_BASE_PATH/pericog/                        /srv/etc/;
-	local_push                 $TM_BASE_PATH/auth/                           /srv/auth/;
-	local_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ /srv/lib/;
-}
-local_pericog_db_init() {
-	read -s -p "Enter password: " password
-
-	cat "$TM_BASE_PATH/util/pericog.sql" |
-	sed "s/\$PW_TWEET2VEC/"$( cat $TM_BASE_PATH/auth/mysql/tweet2vec.pw )"/g" |
-	sed "s/\$PW_PERICOG/"$(   cat $TM_BASE_PATH/auth/mysql/pericog.pw   )"/g" |
-	sudo mysql -u root -p$password;
-}
-local_pericog_init() {
-	local_pericog_db_init;
-	local_pericog_push;
-}
 
 tweets_usa() {
 	mysql -u root -p -h tweets-usa.thisminute.org \
-		--ssl-ca=$TM_BASE_PATH/auth/ssl/tweets_usa/server-ca.pem \
-		--ssl-cert=$TM_BASE_PATH/auth/ssl/tweets_usa/client-cert.pem \
-		--ssl-key=$TM_BASE_PATH/auth/ssl/tweets_usa/client-key.pem;
+		--ssl-ca=$TM_BASE_PATH/auth/ssl/tweets-usa/server-ca.pem \
+		--ssl-cert=$TM_BASE_PATH/auth/ssl/tweets-usa/client-cert.pem \
+		--ssl-key=$TM_BASE_PATH/auth/ssl/tweets-usa/client-key.pem;
 }
 tweets_usa_db_init() {
 	cat "$TM_BASE_PATH/util/tweets.sql" |
@@ -83,7 +78,7 @@ tweets_usa_db_init() {
 	sed "s/\$PW_PERICOG/"$(   cat $TM_BASE_PATH/auth/mysql/pericog.pw   )"/g" |
 
 	mysql -v -u root -p -h tweets-usa.thisminute.org \
-		--ssl-ca=$TM_BASE_PATH/auth/ssl/tweets_usa/server-ca.pem \
-		--ssl-cert=$TM_BASE_PATH/auth/ssl/tweets_usa/client-cert.pem \
-		--ssl-key=$TM_BASE_PATH/auth/ssl/tweets_usa/client-key.pem;
+		--ssl-ca=$TM_BASE_PATH/auth/ssl/tweets-usa/server-ca.pem \
+		--ssl-cert=$TM_BASE_PATH/auth/ssl/tweets-usa/client-cert.pem \
+		--ssl-key=$TM_BASE_PATH/auth/ssl/tweets-usa/client-key.pem;
 }
