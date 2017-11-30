d9af04c3b9c148e8a64a0f598f26c95a561084fb
Timofey Kondrashov <tkondrashov@gmail.com>
2017-11-30 10:03:03 -0500

    add API for tweet sampling

---
+++ html/sentinel/get_tweet.php
@@ -0,0 +1,27 @@
+<?php
+$config = parse_ini_file("/srv/config.ini", true);
+$target = $config['connections'][$config['connections']['active']];
+$db = new mysqli($target, "sentinel", file_get_contents('/srv/auth/mysql/sentinel.pw'), "ThisMinute");
+
+$limit = $_GET['n'] ?: 1;
+$limit = max(min(100, (int)$limit), 1);
+
+$result = [];
+if (!($query = $db->query("SELECT text FROM tweets ORDER BY TIME DESC LIMIT $limit"))) {
+	echo $db->error;
+	die();
+}
+$result = $query->fetch_all(MYSQLI_ASSOC);
+$query->close();
+
+$texts = [];
+foreach ($result as $row) {
+	$texts []= $row['text'];
+}
+
+if ($_GET['format']) {
+	echo implode("<br>", $texts);
+} else {
+	echo json_encode($texts);
+}
+$db->close();
