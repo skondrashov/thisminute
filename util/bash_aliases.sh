77e37c545317330dad99660d90a78b2b896368f2
Timofey Kondrashov <tkondrashov@gmail.com>
2017-12-19 21:58:02 -0500

    UNIT TESTS

--- util/bash_aliases.sh
+++ util/bash_aliases.sh
@@ -23,25 +23,25 @@ sentinel_push() {
 
 archivist() { ssh -i $TM_KEY_PATH $TM_ARCHIVIST_ADDRESS; }
 archivist_push() {
-    tm_push                 $TM_BASE_PATH/config.ini     $TM_ARCHIVIST_ADDRESS:/srv/config.ini;
-    tm_push --exclude 'lib' $TM_BASE_PATH/archivist/     $TM_ARCHIVIST_ADDRESS:/srv/etc/;
-    tm_push                 $TM_BASE_PATH/auth/          $TM_ARCHIVIST_ADDRESS:/srv/auth/;
-    tm_push                 $TM_BASE_PATH/archivist/lib/ $TM_ARCHIVIST_ADDRESS:/srv/lib/;
+    echo "writing /srv/config.ini"; tm_push                 $TM_BASE_PATH/config.ini     $TM_ARCHIVIST_ADDRESS:/srv/config.ini;
+    echo "writing /srv/etc/";       tm_push --exclude 'lib' $TM_BASE_PATH/archivist/     $TM_ARCHIVIST_ADDRESS:/srv/etc/;
+    echo "writing /srv/auth/";      tm_push                 $TM_BASE_PATH/auth/          $TM_ARCHIVIST_ADDRESS:/srv/auth/;
+    echo "writing /srv/lib/";       tm_push                 $TM_BASE_PATH/archivist/lib/ $TM_ARCHIVIST_ADDRESS:/srv/lib/;
 }
 
 pericog() { ssh -i $TM_KEY_PATH $TM_PERICOG_ADDRESS; }
 pericog_push() {
     if [ $TM_PERICOG_ADDRESS == "localhost" ]
     then
-        tm_local_push                 $TM_BASE_PATH/config.ini                      /srv/config.ini;
-        tm_local_push --exclude 'lib' $TM_BASE_PATH/pericog/                        /srv/etc/;
-        tm_local_push                 $TM_BASE_PATH/auth/                           /srv/auth/;
-        tm_local_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ /srv/lib/;
+        echo "writing /srv/config.ini"; tm_local_push                 $TM_BASE_PATH/config.ini                      /srv/config.ini;
+        echo "writing /srv/etc/";       tm_local_push --exclude 'lib' $TM_BASE_PATH/pericog/                        /srv/etc/;
+        echo "writing /srv/auth/";      tm_local_push                 $TM_BASE_PATH/auth/                           /srv/auth/;
+        echo "writing /srv/lib/";       tm_local_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ /srv/lib/;
     else
-        tm_push                 $TM_BASE_PATH/config.ini                      $TM_PERICOG_ADDRESS:/srv/config.ini;
-        tm_push --exclude 'lib' $TM_BASE_PATH/pericog/                        $TM_PERICOG_ADDRESS:/srv/etc/;
-        tm_push                 $TM_BASE_PATH/auth/                           $TM_PERICOG_ADDRESS:/srv/auth/;
-        tm_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ $TM_PERICOG_ADDRESS:/srv/lib/;
+        echo "writing /srv/config.ini"; tm_push                 $TM_BASE_PATH/config.ini                      $TM_PERICOG_ADDRESS:/srv/config.ini;
+        echo "writing /srv/etc/";       tm_push --exclude 'lib' $TM_BASE_PATH/pericog/                        $TM_PERICOG_ADDRESS:/srv/etc/;
+        echo "writing /srv/auth/";      tm_push                 $TM_BASE_PATH/auth/                           $TM_PERICOG_ADDRESS:/srv/auth/;
+        echo "writing /srv/lib/";       tm_push                 $TM_BASE_PATH/lib/ $TM_BASE_PATH/pericog/lib/ $TM_PERICOG_ADDRESS:/srv/lib/;
     fi
 }
 pericog_init() {
@@ -73,20 +73,34 @@ pericog_init() {
     pericog_push;
 }
 
-tweets_usa() {
-    mysql -u root -p -h tweets-usa.thisminute.org \
-        --ssl-ca=$TM_BASE_PATH/auth/ssl/tweets-usa/server-ca.pem \
-        --ssl-cert=$TM_BASE_PATH/auth/ssl/tweets-usa/client-cert.pem \
-        --ssl-key=$TM_BASE_PATH/auth/ssl/tweets-usa/client-key.pem;
+tm_tweets() {
+    if [ -z "$1" ]
+    then
+        echo "Example usage: tm_tweets usa"
+        return
+    fi
+
+    mysql -u root -p -h tweets-$1.thisminute.org;
+     # \
+     #    --ssl-ca=$TM_BASE_PATH/auth/ssl/tweets/server-ca.pem \
+     #    --ssl-cert=$TM_BASE_PATH/auth/ssl/tweets/client-cert.pem \
+     #    --ssl-key=$TM_BASE_PATH/auth/ssl/tweets/client-key.pem;
 }
-tweets_usa_db_init() {
+tm_tweets_init() {
+    if [ -z "$1" ]
+    then
+        echo "Example usage: tm_tweets_init usa"
+        return
+    fi
+
     cat "$TM_BASE_PATH/util/tweets.sql" |
     sed "s/\$PW_SENTINEL/"$(  cat $TM_BASE_PATH/auth/mysql/sentinel.pw  )"/g" |
     sed "s/\$PW_ARCHIVIST/"$( cat $TM_BASE_PATH/auth/mysql/archivist.pw )"/g" |
     sed "s/\$PW_PERICOG/"$(   cat $TM_BASE_PATH/auth/mysql/pericog.pw   )"/g" |
 
-    mysql -v -u root -p -h tweets-usa.thisminute.org \
-        --ssl-ca=$TM_BASE_PATH/auth/ssl/tweets-usa/server-ca.pem \
-        --ssl-cert=$TM_BASE_PATH/auth/ssl/tweets-usa/client-cert.pem \
-        --ssl-key=$TM_BASE_PATH/auth/ssl/tweets-usa/client-key.pem;
+    mysql -vu root -p -h tweets-$1.thisminute.org;
+     # \
+     #    --ssl-ca=$TM_BASE_PATH/auth/ssl/tweets/server-ca.pem \
+     #    --ssl-cert=$TM_BASE_PATH/auth/ssl/tweets/client-cert.pem \
+     #    --ssl-key=$TM_BASE_PATH/auth/ssl/tweets/client-key.pem;
 }
