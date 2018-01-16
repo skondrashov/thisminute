7321e9eacddc7711bda5ea8fb840533a5a46218d
Timofey Kondrashov <tkondrashov@gmail.com>
2018-01-15 22:05:15 -0500

    django init

---
+++ sentinel/html/sentinel/get_markers.php
@@ -0,0 +1,18 @@
+<?php
+$config = parse_ini_file("/srv/config.ini", true);
+$target = $config['connections'][$config['connections']['active']];
+$db = new mysqli($target, "sentinel", file_get_contents('/srv/auth/mysql/sentinel.pw'), "ThisMinute");
+
+$result = [];
+if (!($query = $db->query("SELECT * FROM events;")))
+	die();
+$result["events"] = $query->fetch_all(MYSQLI_ASSOC);
+$query->close();
+
+if (!($query = $db->query("SELECT * FROM event_tweets ORDER BY time ASC;")))
+	die();
+$result["tweets"] = $query->fetch_all(MYSQLI_ASSOC);
+$query->close();
+
+echo json_encode($result);
+$db->close();
