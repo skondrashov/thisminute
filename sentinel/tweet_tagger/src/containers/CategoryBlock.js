c9160cea060bd33103d22d14266aebaf270c47bd
mawills <wattmills@gmail.com>
2018-01-30 12:04:25 -0500

    remove useless renderOverlay()

--- sentinel/tweet_tagger/src/containers/CategoryBlock.js
+++ sentinel/tweet_tagger/src/containers/CategoryBlock.js
@@ -27,30 +27,8 @@ class CategoryBlock extends Component {
     }
   }
 
-  renderOverlay(color) {
-    return(
-      <div style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        height: '100%',
-        width: '100%',
-        zIndex: 1,
-        opacity: 0.5,
-        backgroundColor: color
-      }} />
-    );
-  }
-
   renderTweetBlocks() {
     return this.state.tweets.map((tweet) => {
-      const t = {};
-      /*for each (stateTweet in state.tweets) {
-        if(stateTweet.post_id == tweet.id) {
-          console.log(stateTweet);
-          t = stateTweet;
-        }
-      }*/
       return <TweetBlock key={tweet.tweet_id} tweet={tweet} />;
     });
   }
@@ -59,16 +37,16 @@ class CategoryBlock extends Component {
     const { connectDropTarget, isOver, canDrop } = this.props;
 
     return connectDropTarget(
-      <div className="grid category-list" style={{
-        position: 'relative',
-        width: '100%',
-        height: '100%'
+      <div
+        className="grid category-list"
+        style={{
+          opacity: canDrop ? 0.5 : 1,
+          backgroundColor: canDrop ? 'yellow' : ''
       }}>
         <div className="text-center">
           <h3>{this.props.categoryName}</h3>
         </div>
         {this.renderTweetBlocks()}
-        {isOver && canDrop && this.renderOverlay('yellow')}
       </div>
     );
   }
