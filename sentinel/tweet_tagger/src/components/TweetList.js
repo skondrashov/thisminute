f864e8a75456bc845221caf93b696830b87f1d01
mawills <wattmills@gmail.com>
2018-02-23 09:41:04 -0500

    add tweets on pageload

--- sentinel/tweet_tagger/src/components/TweetList.js
+++ sentinel/tweet_tagger/src/components/TweetList.js
@@ -2,11 +2,29 @@ import React, { Component } from 'react';
 import TweetBlock from './TweetBlock';
 import axios from 'axios';
 
+const MAX_NUM_TWEETS = 5;
+
 class TweetList extends Component {
   constructor(props) {
     super(props);
 
     this.state = { tweets: [] };
+  }
+
+  componentDidMount(){
+    const url = 'http://thisminute.org/sentinel/get_tweet.php?n=5';
+
+    axios.get(url)
+      .then((response) => {
+        var newTweets = [];
+        for(var tweet of response.data) {
+          console.log(tweet);
+          let newTweet = { id: Math.random(), content: tweet };
+          newTweets.push(newTweet);
+        }
+        console.log(newTweets);
+        this.setState({ tweets: this.state.tweets.concat(newTweets) });
+      })
   }
 
   // TODO: Use a better search algo to find the element to remove
@@ -29,19 +47,23 @@ class TweetList extends Component {
 
 // TODO: Get IDs for tweets from database, not Math.random
   _getNewTweet() {
-    const url = 'http://thisminute.org/sentinel/get_tweet.php?n=1';
+    if(this.state.tweets.length < MAX_NUM_TWEETS) {
+      const url = 'http://thisminute.org/sentinel/get_tweet.php?n=1';
 
-    axios.get(url)
-      .then((response) => {
-        this.setState({ tweets: [...this.state.tweets, { id: Math.random(), content: response.data[0] }] });
-      })
+      axios.get(url)
+        .then((response) => {
+          this.setState({ tweets: [...this.state.tweets, { id: Math.random(), content: response.data[0] }] });
+        });
+    }
   }
 
   render() {
     return (
       <div className="tweet-list">
-        <button onClick={event => this._getNewTweet()}>Get Tweet</button>
         {this._renderTweetBlocks()}
+        <div>
+          <button onClick={event => this._getNewTweet()}>Get Tweet</button>
+        </div>
       </div>
     );
   }
