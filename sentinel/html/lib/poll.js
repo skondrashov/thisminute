04575202db0466d8df497bb1db9ebbd055079f85
Timofey Kondrashov <tkondrashov@gmail.com>
2018-06-10 02:36:35 -0400

    remove unused hashcode

--- sentinel/html/lib/poll.js
+++ sentinel/html/lib/poll.js
@@ -54,17 +54,6 @@ function createMarker(lon, lat, text, id, tweets) {
 	return marker;
 }
 
-String.prototype.hashCode = function() {
-	var hash = 0;
-	if (this.length == 0) return hash;
-	for (i = 0; i < this.length; i++) {
-		char = this.charCodeAt(i);
-		hash = ((hash<<5)-hash)+char;
-		hash = hash & hash; // Convert to 32bit integer
-	}
-	return hash;
-}
-
 function poll() {
 	$.ajax({
 		url: "sentinel/get_markers.php"
