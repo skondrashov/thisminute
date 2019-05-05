f016add5c3a2bb583772c309e63f6321d48fac8a
Timofey Kondrashov <tkondrashov@gmail.com>
2019-05-05 01:10:04 -0400

    made voting work

--- website/src/tweet.js
+++ website/src/tweet.js
@@ -1,43 +1,59 @@
 /* globals tm, google */
 import React, { Component } from 'react';
 
-import Votes from './votes';
-
-tm.markers = {};
+import Vote from './vote';
 
 export default class extends Component {
 	constructor(props) {
 		super(props);
 
-		// this.state = {
-		// 	marker: new google.maps.Marker({
-		// 		position: {lat: parseFloat(props.lat), lng: parseFloat(props.lon)},
-		// 		map: tm.map,
-		// 		icon: tm.highlighted_icon,
-		// 	})
-		// };
-
-		let marker = new google.maps.Marker({
+		this.state = {
+			id: props.id,
+			marker: new google.maps.Marker({
 				position: {lat: parseFloat(props.lat), lng: parseFloat(props.lng)},
 				map: tm.map,
 				icon: tm.highlighted_icon,
-			});
-
-		tm.markers[props.id] = marker;
+			}),
+			votes: {},
+		};
 	}
 
 	componentWillUnmount() {
-		// this.state.marker.setMap(null);
+		this.state.marker.setMap(null);
+	}
+
+	vote(id, sentiment, value) {
+		fetch("/api/vote", {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+			},
+			body: JSON.stringify({
+				id: id,
+				[sentiment]: value,
+			}),
+		});
 	}
 
 	render() {
 		return (
 			<div className="tweet">
-				<div className="x vote-only hidden">x</div>
+				<div
+					className="x vote-only hidden"
+					onClick={() => this.vote(this.state.id, "submit", true)}
+				>
+					x
+				</div>
 				<div className="text">
 					{this.props.text}
 				</div>
-				<Votes/>
+				{tm.sentiments.map(sentiment =>
+					<Vote
+						key={sentiment.key}
+						sentiment={sentiment}
+						vote={(sentiment, value) => this.vote(this.state.id, sentiment, value)}
+					/>
+				)}
 			</div>
 		);
 	}
