dac33c531969cb04e721a5b2d5192b31856c783d
Sasha Kondrashov <timofey.kondrashov@vroom.com>
2021-11-28 18:04:48 +0100

    fix stuff

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,10 @@ const path = require("path");
 module.exports = {
   mode: "development",
   entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
   plugins: [
     new CopyWebpackPlugin({
       patterns: ["target/html/index.html"],
